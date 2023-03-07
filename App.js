a6ba073710293c92907dcfaf940daa5f2b826914
miogy <jjangrl87@gmail.com>
2023-03-07 23:32:12 +0900

    Add: latout

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 48 }}>Native Start!</Text>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView horizontal contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny day</Text>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "tomato",
   },
+  city: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  cityName: {
+    fontSize: 58,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  weather: {},
+  day: {},
+  temp: {},
+  description: {},
 });
